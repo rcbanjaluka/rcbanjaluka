bfc8831fbab55d1f3e655a5b427f47b962edf326
rcbanjaluka <nikrad60@gmail.com>
2026-05-10 21:20:28 +0200

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -45,7 +45,6 @@ module.exports = function(eleventyConfig) {
       input: ".",
       output: "_site",
       includes: "_includes",
-      data: "_data"
     },
     htmlTemplateEngine: "njk",
     markdownTemplateEngine: "njk"
